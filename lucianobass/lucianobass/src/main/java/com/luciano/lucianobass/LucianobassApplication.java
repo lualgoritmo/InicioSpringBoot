c33add5d8cb44c66f733fafad5891208ac4f835f
Luciano <lualgoritmo@outlook.com>
2023-11-18 22:51:30 -0300

    Inicio Spring Boot

---
+++ lucianobass/lucianobass/src/main/java/com/luciano/lucianobass/LucianobassApplication.java
@@ -0,0 +1,15 @@
+package com.luciano.lucianobass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucianobassApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LucianobassApplication.class, args);
+		System.out.println("Hello Word! Executando");
+	}
+
+}
---
+++ lucianobass/lucianobass/src/test/java/com/luciano/lucianobass/LucianobassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.lucianobass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucianobassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
