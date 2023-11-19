08b0edb96be507f7be5dbd479bdc724b65361113
Luciano <lualgoritmo@outlook.com>
2023-11-19 13:12:34 -0300

    Projeto Spring Boot Hello Word, feito

---
+++ criarhelloword/criarhelloword/src/main/java/com/luciano/projetoheloword/criarhelloword/CriarhellowordApplication.java
@@ -0,0 +1,14 @@
+package com.luciano.projetoheloword.criarhelloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriarhellowordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriarhellowordApplication.class, args);
+		System.out.println("Hello Word, executando");
+	}
+
+}
---
+++ criarhelloword/criarhelloword/src/test/java/com/luciano/projetoheloword/criarhelloword/CriarhellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.projetoheloword.criarhelloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriarhellowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
