a8073e9c223d84b1940c233da5b15a9876c21111
Luciano <lualgoritmo@outlook.com>
2023-11-18 23:20:54 -0300

    Classe Category

---
+++ lucianobass/lucianobass/src/main/java/com/luciano/lucianobass/entites/Category.java
@@ -0,0 +1,46 @@
+package com.luciano.lucianobass.entites;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+    private long id;
+    private String name;
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+}
