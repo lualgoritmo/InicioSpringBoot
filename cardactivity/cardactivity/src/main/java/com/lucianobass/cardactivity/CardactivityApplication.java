991031834d396e5efb7f59f30090b8608b6aa321
Luciano <lualgoritmo@outlook.com>
2023-11-21 12:07:17 -0300

    Criar conexão com o BD, criar as classes no Resource

---
+++ cardactivity/cardactivity/src/main/java/com/lucianobass/cardactivity/CardactivityApplication.java
@@ -0,0 +1,15 @@
+package com.lucianobass.cardactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardactivityApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CardactivityApplication.class, args);
+		System.out.println("Hello Word! Funcionando");
+	}
+
+}
---
+++ cardactivity/cardactivity/src/test/java/com/lucianobass/cardactivity/CardactivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianobass.cardactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardactivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
