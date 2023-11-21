7d27a3eebfac0f9c58862d36c5eadfd8d6947148
Luciano <lualgoritmo@outlook.com>
2023-11-21 14:38:27 -0300

    Entity criada

---
+++ cardactivity/cardactivity/src/main/java/com/lucianobass/cardactivity/modelsentitys/Card.java
@@ -0,0 +1,82 @@
+package com.lucianobass.cardactivity.modelsentitys;
+
+import jakarta.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_card")
+public class Card implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String numberCard;
+    private String dateFinal;
+    private String codSegurance;
+
+    public Card(Long id, String name, String numberCard, String dateFinal, String codSegurance) {
+        this.id = id;
+        this.name = name;
+        this.numberCard = numberCard;
+        this.dateFinal = dateFinal;
+        this.codSegurance = codSegurance;
+    }
+
+    public Card() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumberCard() {
+        return numberCard;
+    }
+
+    public void setNumberCard(String numberCard) {
+        this.numberCard = numberCard;
+    }
+
+    public String getDateFinal() {
+        return dateFinal;
+    }
+
+    public void setDateFinal(String dateFinal) {
+        this.dateFinal = dateFinal;
+    }
+
+    public String getCodSegurance() {
+        return codSegurance;
+    }
+
+    public void setCodSegurance(String codSegurance) {
+        this.codSegurance = codSegurance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return Objects.equals(id, card.id);
+    }
+}
