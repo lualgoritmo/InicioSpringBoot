6475bd0a928d8bb3e78d72e3c67893e5ffc536ef
Rodrigo <oliboni@unochapeco.edu.br>
2023-12-12 09:02:08 -0300

    add comments

--- cardactivity/cardactivity/src/main/java/com/lucianobass/cardactivity/controllerresources/controller/CardHolderController.java
+++ cardactivity/cardactivity/src/main/java/com/lucianobass/cardactivity/controllerresources/controller/CardHolderController.java
@@ -25,19 +25,37 @@ public class CardHolderController {
     @ResponseStatus(code = HttpStatus.CREATED)
     public CardHolderDTO createCardHolder(@RequestBody CardHolderDTO cardHolderDTO) {
         CardHolder createCardHolder = cardHolderService.createCard(convertDTOToCardHolder(cardHolderDTO));
+        //Essa linha é redundante, então você poderia retornar direto o "convertToResponseDTO(createCardHolder) como no exemplo abaixo"
         CardHolderDTO responseCardHolderDTO = convertToResponseDTO(createCardHolder);
         return responseCardHolderDTO;
     }
+
+//    @PostMapping
+//    @ResponseStatus(code = HttpStatus.CREATED)
+//    public CardHolderDTO createCardHolder(@RequestBody CardHolderDTO cardHolderDTO) {
+//        CardHolder createCardHolder = cardHolderService.createCard(convertDTOToCardHolder(cardHolderDTO));
+//        return convertToResponseDTO(createCardHolder);
+//    }
 
     @GetMapping
     @ResponseStatus(code = HttpStatus.OK)
     public List<CardHolderDTO> getAllCardHolder() {
         List<CardHolder> listCardHolder = cardHolderService.getAllCardsHolders();
+        //A mesma coisa aqui, você está convertendo um objeto e então retornando ele, mas você consegue retornar diretamente a conversão
         List<CardHolderDTO> responseListCardHolderDTO = listCardHolder.stream()
                 .map(ModelMapper::convertToResponseDTO)
                 .collect(Collectors.toList());
         return responseListCardHolderDTO;
     }
+
+//    @GetMapping
+//    @ResponseStatus(code = HttpStatus.OK)
+//    public List<CardHolderDTO> getAllCardHolder() {
+//        List<CardHolder> listCardHolder = cardHolderService.getAllCardsHolders();
+//        return listCardHolder.stream()
+//          .map(MapperConvert::convertToResponseDTO)
+//          .collect(Collectors.toList());
+//    }
 
     @GetMapping("/cardholder/{documentNumber}")
     @ResponseStatus(code = HttpStatus.OK)
@@ -47,11 +65,12 @@ public class CardHolderController {
     }
 
     @PutMapping("/{id}/update")
-    @ResponseStatus(code = HttpStatus.CREATED)
+    @ResponseStatus(code = HttpStatus.CREATED) //Como você está atualizando um objeto e não criando, o status code que vc deve retornar é um OK
     public CardHolderDTO updateCardHolder(@PathVariable Long id,
+                                          //Aqui entra o caso do dto que comentei, no service você só atualiza o nome, então deveria ter um "CardHolderUpdateRequest" somente com "name" para receber aqui no body
                                           @RequestBody(required = false) CardHolder updatedCardHolder) {
         if (id == null) {
-            throw new IllegalArgumentException("O ID não pode ser nulo para deletar");
+            throw new IllegalArgumentException("O ID não pode ser nulo para atualizar"); //Você está fazendo essa validação no service também, ai fica redundante .. poderia deixar somente lá e remover essa
         }
 
         try {
@@ -61,7 +80,7 @@ public class CardHolderController {
             return convertToResponseDTO(updatedResult);
         } catch (EmptyResultDataAccessException e) {
             throw new CardNotFoundExceptions(id);
-        }
+        } //Esse try/catch deveria ficar dentro do seu service, pois é lá que você faz a tentativa de comunicação com banco de dados
     }
 
     @PatchMapping("/{documentNumber}")
@@ -69,7 +88,10 @@ public class CardHolderController {
     public CardHolderDTO updateCardStatusByDocumentNumber(
             @PathVariable String documentNumber,
             @RequestParam(defaultValue = "false") @RequestBody boolean activate) {
+            //Aqui não deveria ser um "requestParam" mas ter um objeto "updateCardStatusRequest" com "active" do tipo boolean e receber usando o @RequestBody
+            @RequestParam(defaultValue = "false")  boolean activate) {
         CardHolder cardHolder = cardHolderService.updateCardStatusByDocumentNumber(documentNumber, activate);
+        //Esse Validate aqui não é necessário, pois no updateCardStatusByDocumentNumber você busca o usuário de dentro do database. logo se não cair na exception o usuãrio é válido
         validateCardHolder(cardHolder);
         cardHolder.getCard().setCardActive(activate);
         return ModelMapper.convertToResponseDTO(cardHolder);
