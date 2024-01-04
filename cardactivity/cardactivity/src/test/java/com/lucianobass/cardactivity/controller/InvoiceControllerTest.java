027881b40157714565a624033b23ca2bc4b2e4da
Luciano <lualgoritmo@outlook.com>
2024-01-03 22:26:29 -0300

    Teste InvoiceController feito

--- cardactivity/cardactivity/src/test/java/com/lucianobass/cardactivity/controller/InvoiceControllerTest.java
+++ cardactivity/cardactivity/src/test/java/com/lucianobass/cardactivity/controller/InvoiceControllerTest.java
@@ -54,55 +54,60 @@ class InvoiceControllerTest {
     @Test
     @DisplayName("Return an invoice list")
     @Transactional
-    void getInvoices() throws Exception {
+    void getInvoicesTest() throws Exception {
         // GIVEN:
-        // CRIA O CARD HOLDER POIS ELE É A BASE DE TUDO E ENTÃO JÁ SALVA NO BANCO
+        // CRIO O CARD HOLDER POIS ELE É A BASE DE TUDO E ENTÃO JÁ SALVA NO BANCO
         CardHolder cardHolder = cardHolderRepository.save(new CardHolder(
                 "José",
                 "11122233344",
-                "01-10-1952")
+                "1980-07-01")
         );
 
-        // Com cardHolder criado, cria a invoice apenas se houver transações
         List<Transaction> transactions = List.of(
                 new Transaction("Pêra", 10.0f),
                 new Transaction("Maça", 10.0f),
                 new Transaction("Uva", 10.0f)
         );
 
-        if (!transactions.isEmpty()) {
-            // Se houver transações, cria a invoice
-            Invoice invoice =
-                    new Invoice(
-                            30.0f,
-                            "IN_PROGRESS",
-                            transactions,
-                            LocalDate.now(),
-                            LocalDate.now(),
-                            cardHolder.getCard()
-                    );
-            entityManager.persist(invoice);
+        // Com cardHolder criado também, dá para criar a invoice:
+        Invoice invoice = invoiceRepository.save(
+                new Invoice(
+                        30.0f,
+                        "IN_PROGRESS",
+                        transactions,
+                        LocalDate.now(),
+                        LocalDate.now(),
+                        cardHolder.getCard()
+                )
+        );
+        entityManager.persist(invoice);
+        // Crio minha lista de transações, note que antes estava faltando informações nas suas transações, qual era o card dela?
+        Transaction transaction1 = new Transaction("Pêra", 10.0f);
+        Transaction transaction2 = new Transaction("Maça", 10.0f);
+        Transaction transaction3 = new Transaction("Uva", 10.0f);
 
-            // Cria a lista de transações
-            List<Transaction> transactionList = List.of(
-                    new Transaction("Pêra", 10.0f),
-                    new Transaction("Maça", 10.0f),
-                    new Transaction("Uva", 10.0f)
-            );
+        // Faz o set dos card para dentro da transaction (isso é uma melhoria, essa construção poderia ficar dentro do construtor)
+        transaction1.setCard(cardHolder.getCard());
+        transaction2.setCard(cardHolder.getCard());
+        transaction3.setCard(cardHolder.getCard());
 
-            // Faz o set dos card para dentro da transaction
-            transactionList.forEach(transaction -> transaction.setCard(cardHolder.getCard()));
+        // Seta qual é a invoice dessa transaction, também poderia ficar dentro do construtor ... o ideal é evitar o uso do set
+        transaction1.setInvoice(invoice);
+        transaction2.setInvoice(invoice);
+        transaction3.setInvoice(invoice);
 
-            // Seta qual é a invoice dessa transaction
-            transactionList.forEach(transaction -> transaction.setInvoice(invoice));
+        // Como no invoice o valor que você passou (30) não cria a fatura com aquele valor, aqui você seta o valor certo da invoice
+        invoice.setTotal(30.0f); // Essa info é salva pelo transaction.save devido ao cascade = ALL
 
-            // Como no invoice o valor que vc passou (30) não cria a fatura com aquele valor,
-            // aqui vc seta o valor certo da invoice
-            invoice.setTotal(30.0f);
+        // Cria a lista de transações
+        List<Transaction> transactionList = List.of(
+                transaction1,
+                transaction2,
+                transaction3
+        );
 
-            // Após criar, salva tudo no banco
-            transactionRepository.saveAll(transactionList);
-        }
+        // Após criar, salva tudo no banco
+        transactionRepository.saveAll(transactionList);
 
         ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/cards/{cardId}/invoices", cardHolder.getCard().getIdCard())
                 .contentType(MediaType.APPLICATION_JSON));
@@ -110,7 +115,7 @@ class InvoiceControllerTest {
         result.andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.cardHolder.name").value("José"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.cardHolder.documentNumber").value("11122233344"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.cardHolder.birthDate").value("01-10-1952"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.cardHolder.birthDate").value("1980-07-01"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.card.expiration").value("30/02"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.invoices[0].total").value(30.0f))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.invoices[0].status").value("IN_PROGRESS"))
